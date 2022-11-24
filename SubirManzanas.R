3f49370cfdbd0db54755fc3e189bacfa50af4fbb
ConstanzaOpazo <114949566+ConstanzaOpazo@users.noreply.github.com>
2022-11-24 16:51:54 -0300

    Update and rename Conexión.R to SubirManzanas.R

---
+++ SubirManzanas.R
@@ -0,0 +1,22 @@
+#Librerias
+library(DBI)
+library(RPostgres)
+library(sf)
+
+#Leer shape file
+data <- sf::st_read("MANZANAS_INFO.SHP")
+data
+
+#Datos PostgreSQL
+dvr <- RPostgres::Postgres()
+db <- "prueba"  
+host_db <- "postgis"
+db_port <- "5432"
+db_user <- "admin_ejemplo"  
+db_password <- "ejemplo_contraseña"
+
+#Código de conexión
+con <- DBI::dbConnect(dvr, dbname = db, host = host_db, port = db_port,
+                    user = db_user, password = db_password)
+#Se escriben los datos
+sf::st_write(data, con, layer ="Manzanas_R01")
